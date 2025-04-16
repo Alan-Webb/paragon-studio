b8daf336236bad3df97229f93ba3a8ab5490c80a
Alan Webb <webbalan91939@gmail.com>
2025-04-16 01:06:33 +0100

    Update Discover component

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,7 +1,49 @@
+import {useState} from "react";
 import {DISCOVER_CONTENT} from "../constants";
 
 const Discover = () => {
-	return <section id="discover"></section>;
+	const [activeTab, setActiveTab] = useState(DISCOVER_CONTENT[0]);
+
+	return (
+		<section className="bg-stone-50 p-6 text-emerald-950" id="discover">
+			<div className="container mx-auto">
+				<h2 className="my-8 text-center text-4xl font-semibold tracking-tighter">
+					Discover
+				</h2>
+				<div className="flex items-center justify-center space-x-4">
+					{DISCOVER_CONTENT.map((tab) => (
+						<button
+							key={tab.id}
+							onClick={() => setActiveTab(tab)}
+							className={`px-4 py-2 font-semibold tracking-tighter transition-colors lg:text-2xl cursor-pointer ${
+								activeTab.id === tab.id
+									? "border-b-2 border-orange-50 text-orange-500"
+									: "text-emerald-950"
+							}`}>
+							{tab.title}
+						</button>
+					))}
+				</div>
+				<div
+					key={activeTab.id}
+					className="mt-8 flex flex-col items-center lg:flex-row">
+					<div className="p-4 lg:w-1/2">
+						<h2 className="mb-4 text-3xl lg:text-4xl">{activeTab.content}</h2>
+						<p className="text-lg tracking-tighter text-emerald-950 lg:text-2xl">
+							{activeTab.description}
+						</p>
+					</div>
+					<div className="p-4 lg:w-1/2">
+						<img
+							src={activeTab.imgSrc}
+							alt={activeTab.title}
+							className="h-auto w-full rounded-lg"
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Discover;
